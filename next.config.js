869e962e9fdcc2ef13688413dfc979d52a4ce96f
rembrandtcosta <rembrandt.costa@ccc.ufcg.edu.br>
2023-09-04 15:37:14 -0300

    fix(images): fix for images in prod

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['https://miniblog-mocha.vercel.app']
+  }
+}
 
 module.exports = nextConfig
